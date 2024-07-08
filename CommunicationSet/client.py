2fc0d55d2575fde8ffe04ceb051ca532eeb06659
SimLabJW <89232609+SimLabJW@users.noreply.github.com>
2024-07-08 17:01:29 +0900

    unity-python http code client, server

--- CommunicationSet/client.py
+++ CommunicationSet/client.py
@@ -25,16 +25,22 @@ http_session = http_connect()
 
 try:
     while True:
-        # 서버로 메시지 전송
-        send_response = http_send(http_session, 'http://localhost:13158/send', {'message': 'Hello, HTTP'})
+        # 클라이언트에서 서버로 데이터 전송
+        send_data = {
+            "drone_id": "CL12345",
+            "home": 1,
+            "store": 2,
+            "state": "active"
+        }
+        send_response = http_send(http_session, 'http://192.168.50.75:13158/client/send', send_data)
         print(f"Send response: {send_response.status_code}")
 
-        # 서버로부터 응답 받기
-        receive_response = http_receive(http_session, 'http://localhost:13158/receive')
+        # 서버로부터 응답 받기 (유니티가 요청하는 엔드포인트)
+        receive_response = http_receive(http_session, 'http://192.168.50.75:13158/unity/receive')
         print(f"Receive response: {receive_response}")
 
         # 주기적으로 10초 대기 후 반복
-        time.sleep(1)
+        time.sleep(10)
 
 except KeyboardInterrupt:
     # 프로그램 종료 시 세션 닫기
